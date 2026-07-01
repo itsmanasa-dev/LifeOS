8f49920010489c098367e9e206f03d95be81644b
Manasa <manasat433@gmail.com>
2026-07-01 19:41:01 +0530

    Hide Firebase API keys inside gitignored local configuration

--- lib/core/config/firebase_options.dart
+++ lib/core/config/firebase_options.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'firebase_keys.dart';
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
@@ -25,54 +26,54 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: String.fromEnvironment('FIREBASE_API_KEY_WEB', defaultValue: 'AIzaSyDlq5axei_TvTAoZrmDd7VxmB3GDfGYbrE'),
-    appId: String.fromEnvironment('FIREBASE_APP_ID_WEB', defaultValue: '1:634862114444:web:7087ea9efb4aa7ffe40da9'),
-    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID_WEB', defaultValue: '634862114444'),
-    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID_WEB', defaultValue: 'lifeos-80f46'),
-    authDomain: String.fromEnvironment('FIREBASE_AUTH_DOMAIN_WEB', defaultValue: 'lifeos-80f46.firebaseapp.com'),
-    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET_WEB', defaultValue: 'lifeos-80f46.firebasestorage.app'),
-    measurementId: String.fromEnvironment('FIREBASE_MEASUREMENT_ID_WEB', defaultValue: 'G-EPNM2216KG'),
+    apiKey: String.fromEnvironment('FIREBASE_API_KEY_WEB', defaultValue: FirebaseKeys.webApiKey),
+    appId: String.fromEnvironment('FIREBASE_APP_ID_WEB', defaultValue: FirebaseKeys.webAppId),
+    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID_WEB', defaultValue: FirebaseKeys.webMessagingSenderId),
+    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID_WEB', defaultValue: FirebaseKeys.webProjectId),
+    authDomain: String.fromEnvironment('FIREBASE_AUTH_DOMAIN_WEB', defaultValue: FirebaseKeys.webAuthDomain),
+    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET_WEB', defaultValue: FirebaseKeys.webStorageBucket),
+    measurementId: String.fromEnvironment('FIREBASE_MEASUREMENT_ID_WEB', defaultValue: FirebaseKeys.webMeasurementId),
   );
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: String.fromEnvironment('FIREBASE_API_KEY_ANDROID', defaultValue: 'AIzaSyDlq5axei_TvTAoZrmDd7VxmB3GDfGYbrE'),
-    appId: String.fromEnvironment('FIREBASE_APP_ID_ANDROID', defaultValue: '1:634862114444:android:mock-android-id'),
-    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID_ANDROID', defaultValue: '634862114444'),
-    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID_ANDROID', defaultValue: 'lifeos-80f46'),
-    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET_ANDROID', defaultValue: 'lifeos-80f46.firebasestorage.app'),
+    apiKey: String.fromEnvironment('FIREBASE_API_KEY_ANDROID', defaultValue: FirebaseKeys.androidApiKey),
+    appId: String.fromEnvironment('FIREBASE_APP_ID_ANDROID', defaultValue: FirebaseKeys.androidAppId),
+    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID_ANDROID', defaultValue: FirebaseKeys.androidMessagingSenderId),
+    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID_ANDROID', defaultValue: FirebaseKeys.androidProjectId),
+    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET_ANDROID', defaultValue: FirebaseKeys.androidStorageBucket),
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: String.fromEnvironment('FIREBASE_API_KEY_IOS', defaultValue: 'AIzaSyDlq5axei_TvTAoZrmDd7VxmB3GDfGYbrE'),
-    appId: String.fromEnvironment('FIREBASE_APP_ID_IOS', defaultValue: '1:634862114444:ios:mock-ios-id'),
-    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID_IOS', defaultValue: '634862114444'),
-    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID_IOS', defaultValue: 'lifeos-80f46'),
-    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET_IOS', defaultValue: 'lifeos-80f46.firebasestorage.app'),
+    apiKey: String.fromEnvironment('FIREBASE_API_KEY_IOS', defaultValue: FirebaseKeys.iosApiKey),
+    appId: String.fromEnvironment('FIREBASE_APP_ID_IOS', defaultValue: FirebaseKeys.iosAppId),
+    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID_IOS', defaultValue: FirebaseKeys.iosMessagingSenderId),
+    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID_IOS', defaultValue: FirebaseKeys.iosProjectId),
+    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET_IOS', defaultValue: FirebaseKeys.iosStorageBucket),
     iosBundleId: String.fromEnvironment('FIREBASE_IOS_BUNDLE_ID', defaultValue: 'com.example.lifeos'),
   );
 
   static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: String.fromEnvironment('FIREBASE_API_KEY_MACOS', defaultValue: 'AIzaSyDlq5axei_TvTAoZrmDd7VxmB3GDfGYbrE'),
-    appId: String.fromEnvironment('FIREBASE_APP_ID_MACOS', defaultValue: '1:634862114444:ios:mock-macos-id'),
-    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID_MACOS', defaultValue: '634862114444'),
-    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID_MACOS', defaultValue: 'lifeos-80f46'),
-    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET_MACOS', defaultValue: 'lifeos-80f46.firebasestorage.app'),
+    apiKey: String.fromEnvironment('FIREBASE_API_KEY_MACOS', defaultValue: FirebaseKeys.macosApiKey),
+    appId: String.fromEnvironment('FIREBASE_APP_ID_MACOS', defaultValue: FirebaseKeys.macosAppId),
+    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID_MACOS', defaultValue: FirebaseKeys.macosMessagingSenderId),
+    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID_MACOS', defaultValue: FirebaseKeys.macosProjectId),
+    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET_MACOS', defaultValue: FirebaseKeys.macosStorageBucket),
     iosBundleId: String.fromEnvironment('FIREBASE_MACOS_BUNDLE_ID', defaultValue: 'com.example.lifeos'),
   );
 
   static const FirebaseOptions windows = FirebaseOptions(
-    apiKey: String.fromEnvironment('FIREBASE_API_KEY_WINDOWS', defaultValue: 'AIzaSyDlq5axei_TvTAoZrmDd7VxmB3GDfGYbrE'),
-    appId: String.fromEnvironment('FIREBASE_APP_ID_WINDOWS', defaultValue: '1:634862114444:web:7087ea9efb4aa7ffe40da9'),
-    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID_WINDOWS', defaultValue: '634862114444'),
-    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID_WINDOWS', defaultValue: 'lifeos-80f46'),
-    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET_WINDOWS', defaultValue: 'lifeos-80f46.firebasestorage.app'),
+    apiKey: String.fromEnvironment('FIREBASE_API_KEY_WINDOWS', defaultValue: FirebaseKeys.windowsApiKey),
+    appId: String.fromEnvironment('FIREBASE_APP_ID_WINDOWS', defaultValue: FirebaseKeys.windowsAppId),
+    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID_WINDOWS', defaultValue: FirebaseKeys.windowsMessagingSenderId),
+    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID_WINDOWS', defaultValue: FirebaseKeys.windowsProjectId),
+    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET_WINDOWS', defaultValue: FirebaseKeys.windowsStorageBucket),
   );
 
   static const FirebaseOptions linux = FirebaseOptions(
-    apiKey: String.fromEnvironment('FIREBASE_API_KEY_LINUX', defaultValue: 'AIzaSyDlq5axei_TvTAoZrmDd7VxmB3GDfGYbrE'),
-    appId: String.fromEnvironment('FIREBASE_APP_ID_LINUX', defaultValue: '1:634862114444:web:7087ea9efb4aa7ffe40da9'),
-    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID_LINUX', defaultValue: '634862114444'),
-    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID_LINUX', defaultValue: 'lifeos-80f46'),
-    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET_LINUX', defaultValue: 'lifeos-80f46.firebasestorage.app'),
+    apiKey: String.fromEnvironment('FIREBASE_API_KEY_LINUX', defaultValue: FirebaseKeys.linuxApiKey),
+    appId: String.fromEnvironment('FIREBASE_APP_ID_LINUX', defaultValue: FirebaseKeys.linuxAppId),
+    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID_LINUX', defaultValue: FirebaseKeys.linuxMessagingSenderId),
+    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID_LINUX', defaultValue: FirebaseKeys.linuxProjectId),
+    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET_LINUX', defaultValue: FirebaseKeys.linuxStorageBucket),
   );
 }
